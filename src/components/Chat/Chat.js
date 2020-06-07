7e82c0e2e3bad791fc66880575f66053a5d7a406
Jesus123780 <53837840+Jesus123780@users.noreply.github.com>
2020-06-07 18:02:36 -0500

    cuarto

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -17,8 +17,8 @@ const Chat = ({ location }) => {
   const [users, setUsers] = useState('');
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
-  //const ENDPOINT = 'https://franciscochat.herokuapp.com//';
-  const ENDPOINT = 'http://localhost:3000/';
+  const ENDPOINT = 'https://franciscochat.herokuapp.com';
+ // const ENDPOINT = 'http://localhost:3000/';
 
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
